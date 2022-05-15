d3726a4b6c467256aebe6cfe247a3f0244171c3b
SergeyIvanovDevelop <sergey.ivanov.develop@gmail.com>
2022-05-15 18:18:58 +0300

    Add loop sending by timeout list of pending games

--- backend/scripts/handler_frontend.js
+++ backend/scripts/handler_frontend.js
@@ -4,6 +4,14 @@ const MongoDB = require('./interaction_DataBase');
 const gamesID = new Map();
 
 async function onConnect(wsClient) {
+  console.log('New Client.');
+  await MongoDB.ping();
+  var ipAddrConnectedClient = wsClient._socket.remoteAddress;
+  var ipPortConnectedClient = wsClient._socket.remotePort;
+  console.log('IP: ',ipAddrConnectedClient);
+  console.log('Port: ',ipPortConnectedClient);
+  sendUpdatedListPendingGamesTimeout(wsClient);
+
   wsClient.on('message', async function(message) {
     await processClientMessage(wsClient, message);
   });
@@ -14,20 +22,18 @@ async function onConnect(wsClient) {
     console.log('User disconnected');
     console.log('ipClosedConnection: ',ipAddrConnectedClient);
     console.log('portClosedConnection: ',ipPortConnectedClient);
-  });
+    });
 }
-
-
 
 async function onConnectPerform(wsClient) {
   var gameId;
-  console.log('New client in the game');
+  console.log('New Client in Game.');
   var ipAddrConnectedClient = wsClient._socket.remoteAddress;
   var ipPortConnectedClient = wsClient._socket.remotePort;
   console.log('IP: ',ipAddrConnectedClient);
   console.log('Port: ',ipPortConnectedClient);
 
-  wsClient.on('message', async function(message) {
+wsClient.on('message', async function(message) {
     /* обработчик сообщений от клиента */
     gameId = await processClientMessagePerform(wsClient, message);
     if (gameId != "perform")
@@ -36,28 +42,27 @@ async function onConnectPerform(wsClient) {
       console.log('GameId = ', gameId);
     }
     
-  });
+});
+wsClient.on('close', async function() {
+  var ipAddrConnectedClient = wsClient._socket.remoteAddress;
+  var ipPortConnectedClient = wsClient._socket.remotePort;
+  console.log('User disconnected');
+  console.log('ipClosedConnection: ',ipAddrConnectedClient);
+  console.log('portClosedConnection: ',ipPortConnectedClient);
 
-  wsClient.on('close', async function() {
-    var ipAddrConnectedClient = wsClient._socket.remoteAddress;
-    var ipPortConnectedClient = wsClient._socket.remotePort;
-    console.log('User disconnected');
-    console.log('ipClosedConnection: ',ipAddrConnectedClient);
-    console.log('portClosedConnection: ',ipPortConnectedClient);
-
-    // Проверяем, завершена ли игра
-    const gameId_ = gamesID.get(wsClient._socket.remotePort);
-    gamesID.delete(wsClient._socket.remotePort);
-    const collectionName = "Pending_Games";
-    const gameFinished = await MongoDB.isGameFinished(gameId_, collectionName);
-    console.log('gameFinished_test = ', gameFinished);
-    if (!gameFinished) {
-      // Значит соединение оборвалось во время игры и необходимо вернуть деньги (даже если был только один игрок пока в игре)
-      // ...
-      // Записать эти данные в блокчейн !
-      // И чеки возврата тоже, все, все, все
-      console.log('Return money to all users in this game...');
-    }
+  // Проверяем, завершена ли игра
+  const gameId_ = gamesID.get(wsClient._socket.remotePort);
+  gamesID.delete(wsClient._socket.remotePort);
+  const collectionName = "Pending_Games";
+  const gameFinished = await MongoDB.isGameFinished(gameId_, collectionName);
+  console.log('gameFinished_test = ', gameFinished);
+  if (!gameFinished) {
+    // Значит соединение оборвалось во время игры и необходимо вернуть деньги (даже если был только один игрок пока в игре)
+    // ...
+    // Записать эти данные в блокчейн !
+    // И чеки возврата тоже, все, все, все
+    console.log('Return money to all users in this game ...');
+  }
   });
 }
 
@@ -329,8 +334,6 @@ async function sendUpdatedListPendingGamesTimeout(wsClient) {
     }, 10000);
 }
 
-
 exports.onConnect = onConnect;
 exports.onConnectPerform = onConnectPerform;
 exports.onConnprocessClientMessageect = processClientMessage;
-  
